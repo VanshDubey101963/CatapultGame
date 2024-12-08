bf8b3ecbda05e052c133374505f28e9a3e446897
cpp <vanshdubey101963@gmail.com>
2024-12-08 19:46:34 +0530

    added new scene for the first page and a ball sprite for the level scene

--- Frontend/scripts/script.js
+++ Frontend/scripts/script.js
@@ -1,30 +1,32 @@
-const canvas = document.getElementById("gamecanvas");
+const canvas = document.getElementById("gamecanvasforlevel");
 const ctx = canvas.getContext("2d");
 canvas.width = 1400;
-canvas.height = 600 ;
+canvas.height = 600;
 
-let bird = { x: 400, y: 360, radius: 20, color: 'red', vx: 0, vy: 0, isMoving: false ,
+let bird = {
+    x: 385, y: 355, radius: 38, color: 'red', vx: 0, vy: 0, isMoving: false,
+    image: new Image(),
     resetPosition: function () {
-        this.x = 400;
-        this.y = 360;
+        this.x = 385;
+        this.y = 355;
         this.vx = 0;
         this.vy = 0;
         this.isMoving = false;
     },
- };
+};
+bird.image.src = "../assets/sprites/ball.jpg"
 
-let slingShot = {x: 350 , y: 350 , isDragging: false , pullStart: {x: 0 , y : 0}, image: new Image()}
+let slingShot = { x: 350, y: 350, isDragging: false, pullStart: { x: 0, y: 0 }, image: new Image() }
 slingShot.image.src = "../assets/sprites/Slingshot.jpg";
 
 const gravity = 0.4;
 const friction = 0.99;
 
 canvas.addEventListener("mousedown", (e) => {
-    const distance = Math.hypot(e.offsetX - bird.x , e.offsetY - bird.y)
-    if(distance <= bird.radius)
-    {
+    const distance = Math.hypot(e.offsetX - bird.x, e.offsetY - bird.y)
+    if (distance <= bird.radius) {
         slingShot.isDragging = true
-        slingShot.pullStart = {x: e.offsetX , y: e.offsetY}
+        slingShot.pullStart = { x: e.offsetX, y: e.offsetY }
     }
 });
 
@@ -32,7 +34,7 @@ canvas.addEventListener("mousemove", (e) => {
     if (slingShot.isDragging) {
         const dx = e.offsetX - slingShot.x;
         const dy = e.offsetY - slingShot.y;
-        const distance = Math.min(Math.hypot(dx, dy), 100); 
+        const distance = Math.min(Math.hypot(dx, dy), 130);
         const angle = Math.atan2(dy, dx);
 
         bird.x = slingShot.x + Math.cos(angle) * distance;
@@ -55,21 +57,20 @@ canvas.addEventListener("mouseup", (e) => {
     }
 });
 
-document.addEventListener('keydown',(e)=>{
-    if(e.key == 'r')
-    {
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'r') {
         bird.resetPosition();
     }
 })
 
 
 function gameLoop() {
-    ctx.clearRect(0,0,1400,600);
-    ctx.drawImage(slingShot.image, slingShot.x , slingShot.y , 100, 150);
+    ctx.clearRect(0, 0, 1400, 600);
+    ctx.drawImage(slingShot.image, slingShot.x, slingShot.y, 100, 150);
 
     if (slingShot.isDragging) {
         ctx.beginPath();
-        ctx.moveTo(bird.x , bird.y);
+        ctx.moveTo(bird.x, bird.y);
         ctx.lineTo(slingShot.pullStart.x, slingShot.pullStart.y);
         ctx.strokeStyle = 'black';
         ctx.lineWidth = 4;
@@ -77,18 +78,11 @@ function gameLoop() {
         ctx.closePath();
     }
 
-
-    ctx.beginPath();
-    ctx.arc(bird.x, bird.y, bird.radius, 0, Math.PI * 2);
-    ctx.fillStyle = bird.color;
-    ctx.fill();
-    ctx.closePath();
-
-    
+    ctx.drawImage(bird.image , bird.x , bird.y , bird.radius, bird.radius);
 
     if (bird.isMoving) {
         bird.vy += gravity;
-        bird.vx *= friction; 
+        bird.vx *= friction;
         bird.vy *= friction;
 
         bird.x += bird.vx;
